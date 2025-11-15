55b3c6ed1515723326c46f31e4f70bfdba5e86e6
vishvish2 <vishnubarath909@gmail.com>
2025-11-15 15:08:58 +0000

    Created JV plots for light intensity

---
+++ main.py
@@ -0,0 +1,64 @@
+import sys
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def read_csv_exception(csv_file_path):
+    """Convert a csv file to a pandas dataframe with exception handling
+
+        Args:
+            df_file_path (pathlib.PosixPath): file path to csv file
+
+        Returns:
+            df (DataFrame): pandas dataframe
+    """
+    try:
+        df = pd.read_csv(csv_file_path)
+        return df
+    except FileNotFoundError:
+        print(f"Can't find:\n{str(csv_file_path)}")
+        sys.exit()
+
+
+# Pandas dataframe for the csv file data
+df = read_csv_exception("CSV Data File.csv")
+
+# Columns in the dataframe that represent the different light intensities
+intensity_cols = {0: "Light intensity 1%",
+                  4: "Light intensity 3%",
+                  8: "Light intensity 10%",
+                  12: "Light intensity 16%",
+                  16: "Light intensity 25%",
+                  20: "Light intensity 32%",
+                  24: "Light intensity 40%",
+                  28: "Light intensity 50%",
+                  32: "Light intensity 63%",
+                  36: "Light intensity 79%",
+                  40: "Light intensity 93%",
+                  44: "Light intensity 100%",
+                  }
+
+solar_cell_area = 0.045     # Units cm^2
+
+n_colours = 12
+cmap = plt.cm.get_cmap('tab20')  # 20 distinct colors for plotting
+
+for key, value in intensity_cols.items():
+    colour = cmap((key / 4) / n_colours)
+
+    # Voltages
+    voltage_vals = pd.to_numeric(df.iloc[:, key][1:])
+
+    # Convert to mA
+    current_vals = pd.to_numeric(df.iloc[:, (key + 1)][1:]) * 1000
+
+    # Compute J
+    current_density_vals = current_vals / solar_cell_area   # Units mA cm^-2
+    plt.plot(voltage_vals, current_density_vals, color=colour, label=value)
+
+plt.xlabel("Voltage / V")
+plt.ylabel("Current Density / mAcm$^{-2}$")
+plt.title("JV curve of Light Intensities of a Perovskite Solar Cell")
+plt.legend()
+plt.show()
